452b7e018c6cd777d1164a5a22b0d92020f53815
wcgordon1 <wcgordon1@gmail.com>
2024-10-31 17:26:08 -0700

    EMAIL WORKS- SAFE HERE

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,17 @@ import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 import mdx from "@astrojs/mdx";
 import devtoolBreakpoints from "astro-devtool-breakpoints";
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'hybrid',
+  adapter: node({
+    mode: 'standalone'
+  }),
+  server: {
+    port: 3000,
+  },
   markdown: {
     drafts: true,
     shikiConfig: {
